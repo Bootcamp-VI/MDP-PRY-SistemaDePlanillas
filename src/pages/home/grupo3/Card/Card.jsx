4c7fc9e33e07a8769d82bf88b7bc5e295827f6a7
Johann C <joe7hann@proton.me>
2023-07-12 15:13:21 -0500

    chore:card component

--- src/pages/home/grupo3/Card/Card.jsx
+++ src/pages/home/grupo3/Card/Card.jsx
@@ -9,7 +9,7 @@ function Card() {
   };
 
   return (
-    <div className="card" style={{ width: "365px", height: "335px" }}>
+    <div className="card" style={{ minWidth: "365px", height: "335px" }}>
       <div className="card-body bg-danger d-flex flex-column justify-content-between">
         <div className="header d-flex justify-content-between align-items-center">
           <span className="text-white">Empleados</span>
