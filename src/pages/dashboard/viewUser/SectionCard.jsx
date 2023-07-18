d1538f65db28268254de64c900af40949ade0003
Luis Angel Salgueron Prado <salgueronprado18@gmail.com>
2023-07-18 16:30:10 -0500

    Modal config

--- src/pages/dashboard/viewUser/SectionCard.jsx
+++ src/pages/dashboard/viewUser/SectionCard.jsx
@@ -8,14 +8,14 @@ export const SectionCard = ({objetos , tittle}) => {
 
     <h5 className="custom-title">{tittle}</h5>
 
-    <div className="card py-3 px-4 mb-3 rounded-5 mx-2" style={{ height: "300px" }}>
-        {objetos.map((objeto, key) => (
-          <div key={key} className="container">
-            <p className="mb-0">{objeto.nombre}</p>
-            <h5 className="mt-0">{objeto.valor}</h5>
-          </div>
-        ))}
-      </div>
+    <div className="card py-3 px-2 px-md-4 mb-3 rounded-5 mx-2" style={{ maxHeight: "290px", overflow: "auto" }}>
+      {objetos.map((objeto, key) => (
+        <div key={key}>
+          <p className="mb-0 fs-6 text-truncate">{objeto.nombre}</p>
+          <h5 className="mt-0 fs-6 m-2">{objeto.valor}</h5>
+        </div>
+      ))}
+    </div>
     </>
   );
 };
