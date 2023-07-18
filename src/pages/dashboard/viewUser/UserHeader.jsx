f232c170493d2f3cb56086610a3ae5ddd20a342f
Johann C <joe7hann@proton.me>
2023-07-18 16:31:40 -0500

    chore: UserHeader font size changed

--- src/pages/dashboard/viewUser/UserHeader.jsx
+++ src/pages/dashboard/viewUser/UserHeader.jsx
@@ -22,7 +22,9 @@ export const UserHeader = ({ closeModal, EmployeData, index }) => {
           </div>
           {/* <div className="col-8 col-md-11 text-center mx-auto"> */}
           <div className="col-8 col-md-11">
-            <h1 className=" ">Perfil de Empleado</h1>
+            <h5>
+              <strong>Perfil de Empleado</strong>
+            </h5>
           </div>
           <div className="col text-center">
             <button
@@ -40,21 +42,22 @@ export const UserHeader = ({ closeModal, EmployeData, index }) => {
               <div className="text-center">
                 <img
                   src={user_data.foto}
-                  className="img-fluid rounded-4"
+                  style={{ width: "180px" }}
+                  className="img-fluid rounded-1"
                   alt="Foto de perfil"
                 />
               </div>
-              <h5 className="m-0 text-center">DNI:{EmployeData.documento}</h5>
+              <p className="m-0 text-center fs-6">DNI:{EmployeData.documento}</p>
             </div>
             <div className="col-md-8 col-12">
               <div className=" row d-flex align-items-center ">
-                <h3 className="col-md-7 col-10">
+                <p class="col-md-7 col-10 fs-6">
                   {EmployeData.nombre +
                     " " +
                     EmployeData.apellidoPaterno +
                     " " +
                     EmployeData.apellidoMaterno}
-                </h3>
+                </p>
                 <img
                   className="col-md-2 col-2"
                   src={`https://flagsapi.com/${EmployeData.codigoPais}/flat/64.png`}
@@ -62,27 +65,27 @@ export const UserHeader = ({ closeModal, EmployeData, index }) => {
                 ></img>
                 {/* <h3 className="col-md-2 col-2">{EmployeData.pais}</h3> */}
                 <div className="col-md-3 col-12 d-none d-md-block">
-                  <button className="btn btn-secondary  ">
+                  <button className="btn btn-sm btn-secondary  ">
                     Editar <LuEdit />
                   </button>
                 </div>
               </div>
               <div className="row">
-                <h5 className="col-12 mb-2">
+                <p className="col-12 mb-2 fs-6">
                   <MdPerson2 /> {EmployeData.datosLaborales.cargo}
-                </h5>
-                <h5 className="col-12 mb-2">
+                </p>
+                <p className="col-12 mb-2 fs-6">
                   <MdLocationPin />{" "}
                   {EmployeData.datosResidencia.distrito +
                     ", " +
                     EmployeData.datosResidencia.departamento}
-                </h5>
-                <h5 className="col-12 mb-2">
+                </p>
+                <p className="col-12 mb-2 fs-6">
                   <MdAlternateEmail /> {EmployeData.datosContacto.correoEmpresa}
-                </h5>
-                <h5 className="col-12 mb-0">
+                </p>
+                <p className="col-12 mb-0 fs-6">
                   <MdLocalPhone /> {EmployeData.datosContacto.celular}
-                </h5>
+                </p>
               </div>
             </div>
           </div>
