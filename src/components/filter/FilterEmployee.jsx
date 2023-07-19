cd725d57dee0ac83356d7b701ced829e9ac845bc
AndreeCarrillo <u20211b589@upc.edu.pe>
2023-07-19 16:13:57 -0500

    feat: html structure

--- src/components/filter/FilterEmployee.jsx
+++ src/components/filter/FilterEmployee.jsx
@@ -1,7 +1,34 @@
-export const FilterEmployee = ()=> {
-    return (<>
-        <div className="container-fluid border rounded container-lg my-2">
-            <h2>Hola</h2>
-        </div>
-    </>)
+import { MdAddCircleOutline } from "react-icons/md";
+
+const FilterEmployee = ({title}) => {
+
+    return (
+        <>
+            <div className="container border rounded my-2 d-flex flex-column">
+                <h2 className="my-2">{title}</h2>
+                <div className="d-flex flex-row my-2 justify-content-between">    
+                    <div className="d-flex flex-ro col-md-3 md-px-4">
+                        <input type="text" className="form-control form-control-sm p-2" id="searcher" placeholder="Buscar por..."/>
+                        <select className="form-select" id="inputGroupSelect01">
+                            <option>Seleccione</option>
+                            <option>DNI</option>
+                            <option>CE</option>
+                            <option>PP</option>
+                        </select>
+                        <div className="d-inline-flex align-items-center">
+                            <label><b>RxH:</b></label>
+                            <select className="form-select" id="inputGroupSelect01">
+                                <option>Planilla</option>
+                                <option>Practicante</option>
+                                <option>Externo</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button className=""><MdAddCircleOutline></MdAddCircleOutline>Agregar</button>
+                </div>
+            </div>
+        </>
+    )
 }
+
+export default FilterEmployee
--- src/routes/ContentMainRoutes.jsx
+++ src/routes/ContentMainRoutes.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/dashboard/Dashboard";
 import CreateEmployee from "../pages/createEmployee/CreateEmployee";
-// import Concepts from "../pages/concepts/Concepts";
+import Concepts from "../pages/concepts/Concepts";
 
 function ContentMainRoutes() {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard/>} />
       <Route path="/create_employed" element={<CreateEmployee/>} />
-      {/* <Route path="/concepts" element={<Concepts/>}/> */}
+      <Route path="/concepts" element={<Concepts/>}/>
     </Routes>
   );
 }
